36a90cb9527a02f7cc899c094eae8d4c58610c58
wangchengwei <jaune162@126.com>
2016-01-09 11:21:42 +0800

    注解定义

---
+++ src/main/java/com/codestd/spring/cxf/annotation/Endpoint.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2015-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.codestd.spring.cxf.annotation;
+
+/**
+ * 用于暴露WebService服务，通过在类上加入{@code @Endpoint}注解实现服务暴露的目的。
+ * <p>使用此注解的前提是Bean已经加入Spring容器。也就是说Bean在Spring配置文件中配置，或已标注@Service、@Component、@Inject等注解。
+ * @author jaune(WangChengwei)
+ * @since 1.0.0
+ */
+public @interface Endpoint {
+	
+	/**
+	 * 此Endpoint在Spring容器中的ID
+	 * @return
+	 */
+	String id();
+	
+	/**
+	 * 服务发布的地址，应神略服务器地址及端口号和项目路径
+	 * @return
+	 */
+	String address();
+
+}
---
+++ src/main/java/com/codestd/spring/cxf/config/EndpointBeanProcessor.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2015-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.codestd.spring.cxf.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author jaune(WangChengwei)
+ * @since 1.0.0
+ */
+@Component
+public class EndpointBeanProcessor implements 
+	DisposableBean, BeanFactoryPostProcessor, BeanPostProcessor, 
+	ApplicationContextAware{
+
+	private ApplicationContext applicationContext;
+	
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext)
+			throws BeansException {
+		this.applicationContext = applicationContext;
+	}
+
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName)
+			throws BeansException {
+		return bean;
+	}
+
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName)
+			throws BeansException {
+		System.out.println(bean);
+		return bean;
+	}
+
+	@Override
+	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory)
+			throws BeansException {
+		System.out.println(beanFactory);
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		
+	}
+
+}
